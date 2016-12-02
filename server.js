591d623857d8848cccc550ce7e586143b215b4bf
Josh Coles <josh@joshcoles.com>
2016-12-02 01:44:20 +0000

    Update name notification is almost working!

--- server.js
+++ server.js
@@ -1,46 +1,50 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
 const uuid = require('node-uuid');
-
-// Set the port to 4000
 const PORT = 4000;
-
-// Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
+// serving static assets from public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
 
-
-
-
-
 wss.on('connection', (ws) => {
-  console.log('Client connected');
-
+// When server receives a new message
+// Parse message then do something with it
   ws.on('message', function (msg) {
     let parsedMessage = JSON.parse(msg)
 
-    let parseNewData = {
-      id: uuid.v4(parsedMessage.id),
-      content: parsedMessage.content,
-      username: parsedMessage.username
+// for new message posts, give it an incomingMessage type,
+// a unique UUID, stringify it and send it back to every
+// client to be handled.
+    if (parsedMessage.type === "postMessage") {
+
+      let parsedNewData = {
+        id: uuid.v4(parsedMessage.id),
+        content: parsedMessage.content,
+        username: parsedMessage.username,
+        type: "incomingMessage"
+      }
+
+      let stringedParsedNewData = JSON.stringify(parsedNewData)
+      wss.clients.forEach(function each(client) {
+        client.send(stringedParsedNewData);
+      });
+
+// for name update messages, stringify the message
+// and send it back to client do be handled.
+    } else if (parsedMessage.type === "nameUpdate") {
+
+      let stringedParsedNewData = JSON.stringify(parsedMessage)
+      wss.clients.forEach(function each(client) {
+        client.send(stringedParsedNewData);
+      });
     }
-    console.log(uuid.v4(parsedMessage.id));
-    let stringedParseNewData = JSON.stringify(parseNewData)
-    wss.clients.forEach(function each(client) {
-      console.log("what I'm looking for:" + stringedParseNewData);
-      client.send(stringedParseNewData);
-    });
-
   });
-
-
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
 });
 
