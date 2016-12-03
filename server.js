29acadc11520d5326ccf32e8a43feead76f97a86
Josh Coles <josh@joshcoles.com>
2016-12-03 00:46:22 +0000

    Added active user count on right side of header. Removed name change functionality on enter.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const SocketServer = require('ws').Server;
 const uuid = require('node-uuid');
 const PORT = 4000;
 const server = express()
+
 // serving static assets from public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
@@ -12,15 +13,41 @@ const server = express()
 const wss = new SocketServer({ server });
 
 
-wss.on('connection', (ws) => {
-// When server receives a new message
-// Parse message then do something with it
+  wss.on('connection', (ws) => {
+
+// ==========================================================
+// keeping track of number of users online
+// ==========================================================
+
+  let numberOfUsersOnline = {
+    type: "numberOfUsersOnline",
+    numberOfUsersOnline: wss.clients.length
+  }
+
+  console.log("Number of users online is: ", numberOfUsersOnline.numberOfUsersOnline)
+
+  let stringedNumberOfUsersOnline = JSON.stringify(numberOfUsersOnline);
+
+  wss.clients.forEach(function each(client) {
+    client.send(stringedNumberOfUsersOnline);
+  });
+
+
+// ==========================================================
+// When server receives a new message,
+// parse message then do something with it
+// ==========================================================
+
+
   ws.on('message', function (msg) {
     let parsedMessage = JSON.parse(msg)
 
+// ==========================================================
 // for new message posts, give it an incomingMessage type,
 // a unique UUID, stringify it and send it back to every
 // client to be handled.
+// ==========================================================
+
     if (parsedMessage.type === "postMessage") {
 
       let parsedNewData = {
@@ -35,8 +62,11 @@ wss.on('connection', (ws) => {
         client.send(stringedParsedNewData);
       });
 
+// ==========================================================
 // for name update messages, stringify the message
 // and send it back to client do be handled.
+// ==========================================================
+
     } else if (parsedMessage.type === "nameUpdate") {
 
       let stringedParsedNewData = JSON.stringify(parsedMessage)
@@ -45,6 +75,19 @@ wss.on('connection', (ws) => {
       });
     }
   });
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+
+// ==========================================================
+// updating number of users online when connection is closed
+// ==========================================================
+    console.log('Client disconnected');
+    numberOfUsersOnline.numberOfUsersOnline = numberOfUsersOnline.numberOfUsersOnline - 1;
+    console.log("Number of users online is: ", numberOfUsersOnline.numberOfUsersOnline)
+
+    wss.clients.forEach(function each(client) {
+    client.send(stringedNumberOfUsersOnline);
+  });
+
+  });
 });
 
